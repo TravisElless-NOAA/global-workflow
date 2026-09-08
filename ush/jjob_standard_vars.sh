637790ebc3eb125f3ae9c3a2b2b4d19e87c298d6
Travis Elless <travis.j.elless@noaa.gov>
2026-09-08 15:28:21 +0000

    add back fix directories

--- ush/jjob_standard_vars.sh
+++ ush/jjob_standard_vars.sh
@@ -75,7 +75,7 @@ export DATA="${DATA:-${DATAROOT}/${jobid}}"
 ################################################
 # Set the NET COM base path (ROTDIR) and COMROOT
 ################################################
-ver_var="${NET}_var"
+ver_var="${NET}_ver"
 ROTDIR=${ROTDIR:-$(compath.py "${envir}/${NET}/${!ver_var}")}
 export ROTDIR
 unset ver_var
